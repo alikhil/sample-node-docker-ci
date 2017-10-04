24627118e46bec4b779004d81858201f5df7b9be
Alik Khilazhev <alikhil@mail.ru>
2017-10-04 15:10:34 +0300

    Fix hello phrase

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 express()
  .get("*", function(req, res) {
     
-      res.send("hollo vladik");
+      res.send("Continuous integration and deployment lab finished");
  })
  .listen(80, function(err) {
     if (err) {
