bd256d7cdcb2755545ca4a94c93d72e9876fcf86
Alik Khilazhev <alikhil@mail.ru>
2017-10-04 15:05:27 +0300

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ docker stop web || true
 docker rm web || true
 docker rmi alikhil/sample-node-with-ci:current || true
 docker tag alikhil/sample-node-with-ci:latest alikhil/sample-node-with-ci:current
-docker run -d --net app --restart always --name web -p 80:80 alikhil/sample-node-with-ci:current
+docker run -d --restart always --name web -p 80:80 alikhil/sample-node-with-ci:current
 EOF
