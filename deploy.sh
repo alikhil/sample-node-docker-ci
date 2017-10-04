0307f1d5a17c97a844aa097de5cbe91446ad6f21
Alik Khilazhev <alikhil@mail.ru>
2017-10-04 14:13:31 +0300

    First commit

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-docker build -t kostyaurysov/sample-node .
-docker push kostyaurysov/sample-node
+docker build -t alikhil/sample-node-with-ci .
+docker push alikhil/sample-node-with-ci
 
-ssh deploy@35.187.30.81 << EOF
-docker pull kostyaurysov/sample-node:latest
+ssh alikhil@35.187.30.81 << EOF
+docker pull alikhil/sample-node-with-ci:latest
 docker stop web || true
 docker rm web || true
-docker rmi kostyaurysov/sample-node:current || true
-docker tag kostyaurysov/sample-node:latest kostyaurysov/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 kostyaurysov/sample-node:current
+docker rmi alikhil/sample-node-with-ci:current || true
+docker tag alikhil/sample-node-with-ci:latest alikhil/sample-node-with-ci:current
+docker run -d --net app --restart always --name web -p 80:80 alikhil/sample-node-with-ci:current
 EOF
